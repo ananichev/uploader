cef86bde48b96d1f0719197be96a4e5466880bbb
ananichev <ananichev.v.v@gmail.com>
2014-09-27 17:08:38 +0300

    Create main actions in app.rb

--- app.rb
+++ app.rb
@@ -15,19 +15,34 @@ class StoredFile
   default_scope(:default).update(order: [:created_at.desc])
 end
 
+DataMapper.auto_upgrade!
 
 get '/' do
-
+  @files = StoredFile.all
+  erb :list
 end
 
 post '/' do
-
+  tempfile = params['file'][:tempfile]
+  @file = StoredFile.new(filename: params['file'][:filename])
+  @file.save!
+  File.copy(tempfile.path, "./files/#{@file.id}.upload")
+  redirect '/'
 end
 
 get '/:id' do
-
+  @file = StoredFile.get(params[:id])
+  send_file "./files/#{@file.id}.upload", filename: @file.filename, type: 'Application/octet-stream'
+  redirect '/'
 end
 
 get '/:id/delete' do
-
+  StoredFile.get(params[:id]).destroy
+  File.delete("./files/#{params[:id]}.upload")
+  redirect '/'
 end
+
+
+
+
+
